57022bf9b7b0b794429c5731457a5a5ccaf2b791
Grefel Nable <felnable@gmail.com>
2022-12-15 04:18:59 -0500

    Create pages for react router and custom css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Error from "./pages/Error";
+import Home from "./pages/Home";
+import TheApp from "./pages/TheApp";
+import Code from "./pages/Code";
+import Repo from "./pages/Repo";
+import SharedLayout from "./pages/SharedLayout";
+
 function App() {
-  return <div className="bg-violet-900">App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Home />} />
+          <Route path="countries" element={<TheApp />} />
+          <Route path="code" element={<Code />} />
+          <Route path="repo" element={<Repo />} />
+          <Route path="*" element={<Error />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/TheApp.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TheApp = () => {
+  return <div>TheApp</div>;
+};
+
+export default TheApp;

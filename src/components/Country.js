192f22c03e4f84cd587d02a2c048b5d2ab1c0bdb
Grefel Nable <felnable@gmail.com>
2022-12-19 03:28:26 -0500

    Add weather feature in the app..
    It only displays the capital city's weather

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,24 +1,31 @@
 import React from "react";
+import Weather from "./Weather";
 
 const Country = ({ country }) => {
   console.log(country);
   const countryName = country.name.common;
   const languageArray = Object.values(country.languages);
+
   return (
-    <div className="card border border-primary-content h-fit p-4">
-      <h3 className="mb-4">{countryName}</h3>
-      <p>Capital: {country.capital}</p>
-      <p>Population: {country.population.toLocaleString()}</p>
-      <h4 className="mt-4">Languages:</h4>
-      <ul>
-        {languageArray.map((language, index) => {
-          return <li key={index}>{language}</li>;
-        })}
-      </ul>
-      <div className="mt-4">
-        <img className="h-[8rem]" src={country.flags.png} alt="" />
+    <article className="border border-primary-content h-fit p-4 flex flex-wrap justify-between rounded-sm">
+      <div className="max-w-sm">
+        <h3 className="mb-4">{countryName}</h3>
+        <p>Capital: {country.capital}</p>
+        <p>Population: {country.population.toLocaleString()}</p>
+        <p>Continent: {country.continents}</p>
+        <h4 className="mt-4">Languages:</h4>
+        <ul>
+          {languageArray.map((language, index) => {
+            return <li key={index}>{language}</li>;
+          })}
+        </ul>
+        <div className="mt-4">
+          <img className="h-[8rem]" src={country.flags.png} alt="" />
+        </div>
       </div>
-    </div>
+      {/* weather of the current country */}
+      <Weather capital={country.capital} />
+    </article>
   );
 };
 
---
+++ src/components/Weather.js
@@ -0,0 +1,64 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const Weather = ({ capital }) => {
+  // display loading screen while fetching weather data
+  const [isLoaded, setIsLoaded] = useState(false);
+  // current temperature
+  const [temperature, setTemperature] = useState("");
+  // weather description and icon
+  const [weather, setWeather] = useState({
+    description: "",
+    icon: "",
+  });
+
+  // api key
+  const api_key = process.env.REACT_APP_API_KEY;
+  // Open weather url
+  const url = `http://api.openweathermap.org/data/2.5/weather?appid=${api_key}&q=${capital}&units=metric`;
+  // fetch data from openweathermap
+  useEffect(() => {
+    console.log("Fetching weather data");
+    axios
+      .get(url)
+      .then((response) => {
+        const weatherData = response.data;
+        console.log(weatherData);
+        setIsLoaded(true);
+        setTemperature(weatherData.main.temp);
+        setWeather({
+          description: weatherData.weather[0].description,
+          icon: weatherData.weather[0].icon,
+        });
+      })
+      .catch((error) => {
+        setIsLoaded(true);
+        console.log(error);
+      });
+  }, []);
+  // Loading screen
+  if (!isLoaded) {
+    return (
+      <section className="flex flex-col items-center justify-center w-screen h-screen">
+        <p>Weather information is loading. . .</p>
+        <div className="w-[6rem] h-[6rem] border-[5px] border-solid    rounded-full border-transparent border-y-accent animate-spin mt-4"></div>
+      </section>
+    );
+  } else
+    return (
+      <div>
+        <h3>Weather in {capital}</h3>
+        <p>
+          Temperature: <span className="font-bold text-lg">{temperature}</span>{" "}
+          Celsius
+        </p>
+        <img
+          src={`http://openweathermap.org/img/wn/${weather.icon}@2x.png`}
+          alt=""
+        />
+        <p className="capitalize">{weather.description}</p>
+      </div>
+    );
+};
+
+export default Weather;
